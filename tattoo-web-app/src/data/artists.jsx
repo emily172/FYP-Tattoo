d0dd53da85e17d447cd8fe9fabf9982f6d122e3c
emily172 <20092335@mail.wit.ie>
2025-03-16 02:08:25 +0000

    Fixed Filtering option for artists

--- tattoo-web-app/src/data/artists.jsx
+++ tattoo-web-app/src/data/artists.jsx
@@ -1,7 +1,7 @@
 export const artists = [
-    { id: 1, name: "Alice Smith", img:"https://placehold.co/350x200", specialty: "Geometric" },
-    { id: 2, name: "Bob Johnson", image: "https://placehold.co/350x200", specialty: "Blackwork" },
-    { id: 3, name: "Cara White", image: "https://placehold.co/350x200", specialty: "Realism" },
+    { id: 1, name: "Alice Smith", img:"https://placehold.co/350x200", specialty: "Geometric Tattoos" },
+    { id: 2, name: "Bob Johnson", image: "https://placehold.co/350x200", specialty: "Blackwork Tattoos" },
+    { id: 3, name: "Cara White", image: "https://placehold.co/350x200", specialty: "Realism Tattoos" },
   ];
   
 
