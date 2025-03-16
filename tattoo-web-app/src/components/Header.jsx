0c2e4de517159021e288dbf1fa89c222d08b7049
emily172 <20092335@mail.wit.ie>
2025-03-16 18:00:45 +0000

    Added new Gallery component added to navgation

--- tattoo-web-app/src/components/Header.jsx
+++ tattoo-web-app/src/components/Header.jsx
@@ -26,6 +26,9 @@ const Header = () => {
               <Link className="nav-link" to="/tatstyles">Styles</Link>
             </li>
             <li className="nav-item">
+              <Link className="nav-link" to="/gallery">Gallery</Link>
+            </li>
+            <li className="nav-item">
               <Link className="nav-link" to="/artists">Artists</Link>
             </li>
             <li className="nav-item">
