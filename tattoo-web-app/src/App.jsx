fb3e48d1999e4aa54907d11586e63d3e823fb51b
emily172 <20092335@mail.wit.ie>
2025-03-16 02:02:19 +0000

    Improved the Navbar

--- tattoo-web-app/src/App.jsx
+++ tattoo-web-app/src/App.jsx
@@ -5,24 +5,25 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Artists from "./pages/Artists";
-import Login from "./pages/Login"; // Import Login page
-import Signup from "./pages/Signup"; // Import Signup page
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Header /> {/* Header appears on all pages */}
+      {/* Flexbox layout for the whole app */}
+      <div className="d-flex flex-column min-vh-100">
+        <Header /> {/* Always at the top */}
         <main className="flex-grow-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/artists" element={<Artists />} />
-            <Route path="/login" element={<Login />} /> {/* Login route */}
-            <Route path="/signup" element={<Signup />} /> {/* Signup route */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </main>
-        <Footer /> {/* Footer appears on all pages */}
+        <Footer /> {/* Always at the bottom */}
       </div>
     </Router>
   );
--- tattoo-web-app/src/components/Header.jsx
+++ tattoo-web-app/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div className="container">
         <Link className="navbar-brand" to="/">Ink Pots</Link>
         <button
