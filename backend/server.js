956db91dd1b5387de2ded24629508d74da20d442
emily172 <20092335@mail.wit.ie>
2025-04-01 20:06:50 +0100

    Creating a database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
+// Setting up the backend using express - API JSON requests, cors -Cross Origin Resource Sharing and mongoose - DB
 const express = require("express");
 const cors = require("cors");
+//Import Mongoose
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Connect to MongoDB - Loaded from the env file
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected successfully!"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 // Test route
 app.get("/", (req, res) => {
