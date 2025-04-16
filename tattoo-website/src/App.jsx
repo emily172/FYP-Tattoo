e2c3f0205e940cf3293020d308af9ce63811fada
emily172 <20092335@mail.wit.ie>
2025-04-16 04:26:55 +0100

    Updated App.jsx  with Studio Routs

--- tattoo-website/src/App.jsx
+++ tattoo-website/src/App.jsx
@@ -5,13 +5,15 @@ import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import Artists from './pages/Artists';
 import Contact from './pages/Contact';
+import Studio from './pages/Studio'; // User-facing Studio page
+import BlogList from './pages/BlogList'; // Blog list page
+import BlogPost from './pages/BlogPost'; // Single blog post page
 import AdminRegister from './pages/AdminRegister'; // Admin Register
 import AdminLogin from './pages/AdminLogin'; // Admin Login
 import AdminDashboard from './pages/AdminDashboard'; // Admin Dashboard
 import ManageArtists from './pages/ManageArtists'; // Artists management
 import ManageGallery from './pages/ManageGallery'; // Gallery management
-import BlogList from './pages/BlogList'; // Blog List Page
-import BlogPost from './pages/BlogPost'; // Individual Blog Post Page
+import ManageStudio from './pages/ManageStudio'; // Admin Studio management
 import AdminBlogManager from './pages/AdminBlogManager'; // Admin Blog Management
 
 function App() {
@@ -21,11 +23,12 @@ function App() {
       <Routes>
         {/* General User Routes */}
         <Route path="/" element={<Home />} />
+        <Route path="/studio" element={<Studio />} /> {/* Studio page */}
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/artists" element={<Artists />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/blogs" element={<BlogList />} />
-        <Route path="/blogs/:id" element={<BlogPost />} />
+        <Route path="/blogs" element={<BlogList />} /> {/* Blog List Page */}
+        <Route path="/blogs/:id" element={<BlogPost />} /> {/* Single Blog Post Page */}
 
         {/* Admin Routes */}
         <Route path="/register" element={<AdminRegister />} />
@@ -33,7 +36,8 @@ function App() {
         <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/dashboard/artists" element={<ManageArtists />} />
         <Route path="/dashboard/gallery" element={<ManageGallery />} />
-        <Route path="/dashboard/blogs" element={<AdminBlogManager />} /> {/* Moved to dashboard */}
+        <Route path="/dashboard/studio" element={<ManageStudio />} /> {/* Studio management */}
+        <Route path="/dashboard/blogs" element={<AdminBlogManager />} /> {/* Admin Blog Management */}
       </Routes>
     </Router>
   );
