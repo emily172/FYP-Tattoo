5b1407757ebe23f47818455f45b05fef449acafb
emily172 <20092335@mail.wit.ie>
2025-04-16 14:15:35 +0100

    Adding more to the history model

--- server/models/History.js
+++ server/models/History.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const historySchema = new mongoose.Schema({
   year: { type: String, required: true }, // Year of the milestone
   event: { type: String, required: true }, // Description of the milestone/event
+  image: { type: String }, // URL for an image
+  video: { type: String }, // URL for a video
+  testimonial: { type: String }, // Testimonial or quote
+  link: { type: String }, // Additional link for detailed info
 });
 
 module.exports = mongoose.model('History', historySchema);
